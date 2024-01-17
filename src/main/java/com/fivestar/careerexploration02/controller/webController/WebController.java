4904c0765bd45d0801088135edab2f8fcee2036c
GAO <154122167+UserGaoLin@users.noreply.github.com>
2024-01-17 09:36:27 +0800

    1130117 09:35 1.新增nav連結到各職業的html. 2.尚未完成完整的html(今日要再更新) 3.img圖片完整性(待確認)

--- src/main/java/com/fivestar/careerexploration02/controller/webController/WebController.java
+++ src/main/java/com/fivestar/careerexploration02/controller/webController/WebController.java
@@ -1,5 +1,6 @@
 package com.fivestar.careerexploration02.controller.webController;
 
+import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -18,10 +19,6 @@ public class WebController
         return "CustomerService";
     }
 
-    @GetMapping("/job/police")
-    public String police(){
-        return "Octn_PoliceOfficer";
-    }
     @GetMapping("/about")
     public String about(){
         return "aboutUs";
@@ -30,6 +27,47 @@ public class WebController
     public String cooperation(){
         return "cooperation";
     }
+
+    @GetMapping("/job/police")
+    public String police(){
+        return "Octn_PoliceOfficer";
+    }
+
+    @GetMapping("/job/police_en")
+    public String police_en() { return "Octn_PoliceOfficer_en"; }
+
+    @GetMapping("/job/actor")
+    public String actor() { return "Octn_Actor"; }
+
+    @GetMapping("/job/chef")
+    public String chef() { return "Octn_Chef"; }
+
+    @GetMapping("/job/counselor")
+    public String counselor() { return "Octn_Counselor"; }
+
+    @GetMapping("/job/dentist")
+    public String dentist() { return "Octn_Dentist";}
+
+    @GetMapping("/job/doctor")
+    public String doctor() { return "Octn_Doctor";}
+
+    @GetMapping("/job/fashiondesigner")
+    public String fashiondesigner() { return "Octn_FashionDesigner"; }
+
+    @GetMapping("/job/reporter")
+    public String reporter() { return "Octn_Reporter"; }
+
+    @GetMapping("/job/teacher")
+    public String teacher() { return "Octn_Teacher"; }
+
+    @GetMapping("/job/tourguide")
+    public String tourguide() {return "Octn_TourGuide"; }
+
+    @GetMapping("/job/weddingplanner")
+    public String weddingplanner() { return "Octn_WeddingPlanner"; }
+
+    @GetMapping("/job/youtuber")
+    public String youtuber() { return "Octn_Youtuber"; }
 }
 
 
